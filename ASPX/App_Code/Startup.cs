86d94c67eecc62570a4924fef5c037052f1fc446
navraj.singh <navraj.singh@infodartmail.com>
2017-09-25 10:23:51 +0530

    Changed to ASP.net Project

---
+++ ASPX/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CloudBank.Startup))]
+namespace CloudBank
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
